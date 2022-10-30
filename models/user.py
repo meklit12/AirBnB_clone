3f9760a473ef5c821a54b2b2b0a26bdec600e879
Nathaniel81 <nathanield356@gmail.com>
2022-10-30 22:47:01 +0300

    usrMdls

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This module defines a User class that inherits from BaseModel.
+Defines a User class that inherits from BaseModel.
 """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """
-    This user class represents a user object and inherits from
+    Represents a user object and inherits from
     the BaseModel class.
     """
 
