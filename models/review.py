1767ae2ad7a280f4144372e73f2fd91d032b1408
Nathaniel81 <nathanield356@gmail.com>
2022-10-30 22:45:25 +0300

    rvwMdls

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines a class called Review that inherits
+Defines a class called Review that inherits
 from the BaseModel class
 """
 from models.base_model import BaseModel
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """
-    This class represents a Review object and inherits from
+    Represents a Review object and inherits from
     a BaseModel class.
     """
     place_id = ""
