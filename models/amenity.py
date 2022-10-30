59edb369f6de0df7945105a06a5e126563192d08
Nathaniel81 <nathanield356@gmail.com>
2022-10-30 22:40:17 +0300

    Mdlsdc

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines a class called Amenity that inherits
+Defines a class called Amenity that inherits
 from the BaseModel class
 """
 from models.base_model import BaseModel
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """
-    This class represents an Amenity object and inherits from
+    Represents an Amenity object and inherits from
     a BaseModel class.
     """
     name = ""
